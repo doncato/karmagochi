6695ee60bf1e87b82b5482c63062976d98b70294
doncato <doncato@notarobot.cyou>
2024-04-06 21:29:19 +0200

    Expand Classes / Code Structure
    - Add more functions to classes
    - Implement some functions on classes

--- src/class.cpp
+++ src/class.cpp
@@ -4,7 +4,34 @@
  * @file class.cpp
  */
 
+#include <string>
+#include <unordered_map>
+
 #include "class.h"
+
+using namespace std;
+
+Action::Action(string act) : thisAction(AvailableActions::invalid) {
+    static std::unordered_map<std::string, AvailableActions> const table = {
+        {"check-egg", AvailableActions::check_for_egg},
+        {"feed", AvailableActions::feed},
+        {"play", AvailableActions::play},
+        {"put-to-sleep", AvailableActions::put_to_sleep},
+        {"slaughter", AvailableActions::slaughter},
+        {"therapize", AvailableActions::therapize},
+        {"train", AvailableActions::train},
+        {"wakeup", AvailableActions::wakeup},
+    };
+
+    auto it = table.find(act);
+    if (it != table.end()) {
+        this->thisAction = it->second;
+    }
+}
+
+enum AvailableActions Action::get() {
+    return this->thisAction;
+}
 
 
 Effects::Effects(int born, int sick) : born(born), sleep(0), depressive(0), hatching(0), overworked(0), sick(sick) {};
@@ -18,7 +45,14 @@ Effects::Effects(int born, int sleep, int depressive, int hatching, int overwork
 {};
 
 Stats::Stats() : fitness(10), happiness(10), motivation(10), resillience(10), restfulness(10), saturation(10) {};
-Stats::Stats(int fitness, int happiness, int motivation, int resillience, int restfulness, int saturation) :
+Stats::Stats(
+    int_fast8_t fitness,
+    int_fast8_t happiness,
+    int_fast8_t motivation,
+    int_fast8_t resillience,
+    int_fast8_t restfulness,
+    int_fast8_t saturation
+) :
     fitness(fitness),
     happiness(happiness),
     motivation(motivation),
@@ -26,3 +60,54 @@ Stats::Stats(int fitness, int happiness, int motivation, int resillience, int re
     restfulness(restfulness),
     saturation(saturation)
 {};
+
+void Stats::adjustFitness(int_fast8_t val) {this->adjustValue(&this->fitness, val);};
+void Stats::setFitness(int_fast8_t val) {this->setValue(&this->fitness, val);};
+
+void Stats::adjustHappiness(int_fast8_t val) {this->adjustValue(&this->happiness, val);};
+void Stats::setHappiness(int_fast8_t val) {this->setValue(&this->happiness, val);};
+
+void Stats::adjustMotivation(int_fast8_t val) {this->adjustValue(&this->motivation, val);};
+void Stats::setMotivation(int_fast8_t val) {this->setValue(&this->motivation, val);};
+
+void Stats::adjustResillience(int_fast8_t val) {this->adjustValue(&this->resillience, val);};
+void Stats::setResillience(int_fast8_t val) {this->setValue(&this->resillience, val);};
+
+void Stats::adjustRestfulness(int_fast8_t val) {this->adjustValue(&this->restfulness, val);};
+void Stats::setRestfulness(int_fast8_t val) {this->setValue(&this->restfulness, val);};
+
+void Stats::adjustSaturation(int_fast8_t val) {this->adjustValue(&this->saturation, val);};
+void Stats::setSaturation(int_fast8_t val) {this->setValue(&this->saturation, val);};
+
+void Stats::capValue(int_fast8_t *field) {
+    if (*field > 100) {
+        *field = 100;
+    } else if (*field < 0) {
+        *field = 0;
+    };
+};
+
+void Stats::adjustValue(int_fast8_t *field, int_fast8_t value) {
+    *field += value;
+    capValue(field);
+};
+
+void Stats::setValue(int_fast8_t *field, int_fast8_t value) {
+    *field = value;
+    capValue(field);
+}
+
+
+void Entity::performAction(AvailableActions action) {
+    switch (action) {
+        case AvailableActions::check_for_egg  : this->checkForEgg();    break;
+        case AvailableActions::feed           : this->feed();           break;
+        case AvailableActions::play           : this->play();           break;
+        case AvailableActions::put_to_sleep   : this->putToSleep();     break;
+        case AvailableActions::slaughter      : this->slaughter();      break;
+        case AvailableActions::therapize      : this->therapize();      break;
+        case AvailableActions::train          : this->train();          break;
+        case AvailableActions::wakeup         : this->wakeup();         break;
+        default                               : this->invalidAction();  break;
+    }
+};
--- src/class.h
+++ src/class.h
@@ -4,12 +4,13 @@
  * @file class.h
  */
 
-#include <string.h>
+#include <string>
+#include <cstdint>
 
 /**
  * @brief Available actions to interact with the entity
  */
-enum Action {
+enum AvailableActions {
     check_for_egg,
     feed,
     play,
@@ -18,13 +19,29 @@ enum Action {
     therapize,
     train,
     wakeup,
+    invalid
+};
+
+class Action {
+    private:
+        enum AvailableActions thisAction;   
+
+    public:
+        Action(std::string act);
+
+        /**
+         * @brief Gets the chosen action
+         * 
+         * @return enum AvailableActions 
+         */
+        enum AvailableActions get();
 };
 
 /**
  * @brief Effects that can be applied to the entity
  */
 class Effects {
-    public:
+    private:
         int born;
         int sleep;
         int depressive;
@@ -32,6 +49,7 @@ class Effects {
         int overworked;
         int sick;
 
+    public:
         Effects(int born, int sick);
         Effects(int born, int sleep, int depressive, int hatching, int overworked, int sick);
 };
@@ -40,28 +58,103 @@ class Effects {
  * @brief Stats that the entity has
  */
 class Stats {
-    public:
-        int fitness;
-        int happiness;
-        int motivation;
-        int resillience;
-        int restfulness;
-        int saturation;
+    private:
+        int_fast8_t fitness;
+        int_fast8_t happiness;
+        int_fast8_t motivation;
+        int_fast8_t resillience;
+        int_fast8_t restfulness;
+        int_fast8_t saturation;
 
+    public:
         Stats();
-        Stats(int fitness, int happiness, int motivation, int resillience, int restfulness, int saturation);
+        Stats(
+            int_fast8_t fitness,
+            int_fast8_t happiness,
+            int_fast8_t motivation,
+            int_fast8_t resillience,
+            int_fast8_t restfulness,
+            int_fast8_t saturation
+        );
+
+        void adjustFitness(int_fast8_t val);
+        void setFitness(int_fast8_t val);
+
+        void adjustHappiness(int_fast8_t val);
+        void setHappiness(int_fast8_t val);
+
+        void adjustMotivation(int_fast8_t val);
+        void setMotivation(int_fast8_t val);
+
+        void adjustResillience(int_fast8_t val);
+        void setResillience(int_fast8_t val);
+
+        void adjustRestfulness(int_fast8_t val);
+        void setRestfulness(int_fast8_t val);
+
+        void adjustSaturation(int_fast8_t val);
+        void setSaturation(int_fast8_t val);
+
+
+    private:
+        /**
+         * @brief Ensures the value of the given attribute is within limits
+         * 
+         * @param field the value to check
+         */
+        void capValue(int_fast8_t *field);
+
+        /**
+         * @brief Adjust the given field with the given value
+         * 
+         * @param field The field to adjust
+         * @param val the value to adjust with
+         */
+        void adjustValue(int_fast8_t *field, int_fast8_t val);
+
+        /**
+         * @brief Set the Value of the given field
+         * 
+         * @param field the field to set
+         * @param val the value to set on the field
+         */
+        void setValue(int_fast8_t *field, int_fast8_t val);
 };
 
 /**
  * @brief The Entity itself
  */
 class Entity {
-    public:
+    private:
         Stats *thisStats;
         Effects *thisEffects;
-        String thisName;
-        u32 lastUpdate;
-        u32 inStateUntil;
+        std::string thisName;
+        uint32_t lastUpdate;
+        uint32_t inStateUntil;
+
+    public:
 
         Entity();
+        
+        /**
+         * @brief Performs the supplied action on the Entity
+         * @param action the action that is to be performed
+         */
+        void performAction(AvailableActions action);
+
+        void checkForEgg();
+        void feed();
+        void play();
+        void putToSleep();
+        void slaughter();
+        void therapize();
+        void train();
+        void wakeup();
+        void invalidAction();
+
+    private:
+        /**
+         * @brief Updates the 'lastUpdate' variable to now
+        */
+        void updateTime();
 };
